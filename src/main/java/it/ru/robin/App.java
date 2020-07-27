9778bef057bb1fef93fb85667cdf4eb568ff61d3
Alina <amsultanova@it.ru>
2020-07-27 23:05:52 +0500

    Create maven project

---
+++ src/main/java/it/ru/robin/App.java
@@ -0,0 +1,13 @@
+package it.ru.robin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
