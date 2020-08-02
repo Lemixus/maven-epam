6163955637b27d015e706e04f9b93f9f0e8ca912
Alina <amsultanova@it.ru>
2020-08-02 23:48:32 +0500

    Программа приветствует любого пользователя при вводе его имени через командную строку

--- src/main/java/it/ru/robin/App.java
+++ src/main/java/it/ru/robin/App.java
@@ -1,13 +1,17 @@
+
+//1. I can win!
 package it.ru.robin;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        System.out.println( "Hello, "+ name +"!");
     }
 }
