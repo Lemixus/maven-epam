1c735b8fcecb6f3c8c693e2fc1ec9321a1cc2534
Alina <amsultanova@it.ru>
2020-08-03 20:41:20 +0500

    Программа подсчитывает сумму  введенных  и произведение чисел и выводит результат в консоль

--- src/main/java/it/ru/robin/App.java
+++ src/main/java/it/ru/robin/App.java
@@ -2,27 +2,35 @@
 //1. I can win!
 package it.ru.robin;
 
-
 import java.io.BufferedReader;
-
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String array[];
-        String li = reader.readLine();
+        String num = reader.readLine();
         String delimeter = " ";
-        array = li.split(delimeter);
-        for(int i= 0; i < array.length;i++){
-            System.out.println(array[i]);
+        String[] array = num.split(delimeter);
+        int[] massiv = new int[array.length];
+
+        for(int i = 0; i < array.length; i++) {
+
+            massiv[i]= Integer.parseInt(array[i]);
         }
-        for(int i= 0; i < array.length;i++){
-            System.out.print(array[i]+ " ");
+        int summary = 0;
+        int mult = 1;
+
+        for (int i=0; i < massiv.length; i++) {
+            summary += massiv[i];
+            mult *=massiv[i];
+
         }
+
+        System.out.print(summary + "\n" + mult);
+
+
 
 
     }
